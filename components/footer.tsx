653e9eead882c1bd852a6dc25604eb52ba86dcc0
fwoofyFloofy <BISCUITISNOTACOOKIE@protonmail.com>
2022-02-20 03:32:20 +0000

    read commit desc.
    navbar, license, next.config.js and footer fixed so project can fully compile

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export const Footer = () => {
     return (
@@ -25,11 +26,14 @@ export const Footer = () => {
               <Link href="https://github.com/fwuffyboi/cosmos3">GitHub</Link>
             </div>
 
-            <div className="w-1/8 px-2 h-12">
-            <Link href="/license">License</Link>
+            <div className="w-1/8 px-2 h-12 space-x-90">
+              <Link href="/license">License</Link>
             </div>
-            
-            <div className="w-1/8 px-2 h-12">
+
+            <div id="spacer" className="flex-1">
+            </div>
+          
+            <div className="px-2 h-12 ">
               <h1>Icons by </h1>
               <div className="text-blue-400">
                 <Link href="https://icons.getbootstrap.com">
@@ -38,25 +42,25 @@ export const Footer = () => {
               </div>
             </div>
             
-            <div className="text-smn px-2 w-1/8">
+            <div className="text-smn px-2">
 
               <div className="text-blue-400 text-sm">
                 <Link href="https://github.com/JolteonYellow">
                   JolteonYellow
                 </Link>
               </div>
-              <div className="text-blue-400 text-sm">
+              <div className="text-blue-400 text-sm object-right">
                 <Link href="https://github.com/fwuffyboi">
                   Fwuffyboi
                 </Link>
               </div>
             </div>
 
-            <div className="w-1/8 px-2 h-12">
-              <img className="block mx-auto h-12 rounded-full sm:mx-0 sm:shrink-0" src="https://avatars.githubusercontent.com/u/46378904?v=4" alt="JolteonYellow's github profile picture" />
+            <div className="px-2 h-12">
+              <Image className="block mx-auto h-12 rounded-full sm:mx-0 sm:shrink-0" width="48" height="48" src="https://avatars.githubusercontent.com/u/46378904?v=4" alt="JolteonYellow's github profile picture" />
             </div>
-            <div className="w-1/8 px-2 h-12">
-              <img className="block mx-auto h-12 rounded-full sm:mx-0 sm:shrink-0" src="https://avatars.githubusercontent.com/u/73500040?v=4" alt="Fwuffyboi's github profile picture" />
+            <div className="px-2 h-12">
+              <Image className="block mx-auto h-12 rounded-full sm:mx-0 sm:shrink-0" width="48" height="48" src="https://avatars.githubusercontent.com/u/73500040?v=4" alt="Fwuffyboi's github profile picture" />
             </div>
             
           </div>
